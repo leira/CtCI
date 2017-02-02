c18dd17d5325eb849e7ede00160f2b4e450ef2ce
Leira Hua <leirahua@gmail.com>
2017-02-01 20:40:14 -0800

    question 1.5, remote duplication

--- ch01/q1.5/test_isOneChangeAway.py
+++ ch01/q1.5/test_isOneChangeAway.py
@@ -1,33 +1,19 @@
 #!/usr/bin/env python3
 
-def isSameOrReplace(src, dest):
-    for i in range(len(src)):
-        if src[i] != dest[i]:
-            return src[i+1:] == dest[i+1:]
-    return True
-
-def isInsert(src, dest):
-    for i in range(len(src)):
-        if src[i] != dest[i]:
-            return src[i:] == dest[i+1:]
-    return True
-
-def isDelete(src, dest):
-    for i in range(len(dest)):
-        if src[i] != dest[i]:
-            return src[i+1:] == dest[i:]
-    return True
-
 def isOneChangeAway(src, dest):
     lenDiff = len(src) - len(dest)
-    if lenDiff == 0:
-        return isSameOrReplace(src, dest)
-    elif lenDiff == -1:
-        return isInsert(src, dest)
-    elif lenDiff == 1:
-        return isDelete(src, dest)
-    else:
-        return False
+    if lenDiff > 1 or lenDiff < -1:
+        return False;
+
+    for i in range(min(len(src), len(dest))):
+        if src[i] != dest[i]:
+            if lenDiff == 0:    # replace
+                return src[i+1:] == dest[i+1:]
+            elif lenDiff == -1: # insert
+                return src[i:] == dest[i+1:]
+            if lenDiff == 1:    # delete
+                return src[i+1:] == dest[i:]
+    return True # same or one change in the end
 
 
 def test_isOneChangeAway():
