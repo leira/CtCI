9b3d11cdc0cae062225b1ee468b39430859598ac
Leira Hua <leirahua@gmail.com>
2017-02-02 22:43:43 -0800

    Revert "question 1.7, improve?"
    This reverts commit 8a0059e39ecda314602aea888a58b4205f45cc8c.

--- ch01/q1.7/test_rotate.py
+++ ch01/q1.7/test_rotate.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python3
 
-import pdb
-
-def fromIndex(i, j, dem):
-    return dem-1-j, i
+def rotatedIndex(i, j, dem):
+    return j, dem-1-j
 
 def rotate(matrix):
     dem = len(matrix)
     if dem < 2:
         return matrix
 
-    def rotCell(x, y, temp=None):
-        nx, ny = fromIndex(x, y, dem)
-        if temp != None:
-            matrix[x][y] = temp
-        else:
-            matrix[x][y] = matrix[nx][ny]
-        return nx, ny
-
     for i in range(dem//2):
         for j in range(i, dem-1-i):
-            x, y = i, j
             temp = matrix[i][j]
-            for _ in range(3):
-                x, y = rotCell(x, y)
-            x, y = rotCell(x, y, temp)
+            matrix[i][j] = matrix[dem-1-j][i]
+            matrix[dem-1-j][i] = matrix[dem-1-i][dem-1-j]
+            matrix[dem-1-i][dem-1-j] = matrix[j][dem-1-i]
+            matrix[j][dem-1-i] = temp
 
     return matrix
 
