64da7d0742dd3b7c2f61943ed336a64320f0364a
Leira Hua <leirahua@gmail.com>
2017-05-01 17:24:50 -0700

    pure functional version, but with a performance bug

--- ch04/q4.1/isConnected.py
+++ ch04/q4.1/isConnected.py
@@ -1,20 +1,22 @@
-graph = { 1: set([2, 3]),
-          2: set([1, 3, 4, 5]),
-          3: set([1, 2, 4]),
-          4: set([2, 3, 5]),
-          5: set([2, 4]) }
+graph = { 1: [2, 3],
+          2: [1, 3, 4, 5],
+          3: [1, 2, 4],
+          4: [2, 3, 5],
+          5: [2, 4] }
 
 def isConnected(graph, a, b):
-    front = set([a,])
-    seen = set()
-    while len(front):
+    def testConnected(front, seen):
+        print(front, seen)
         if b in front:
-            return True;
-        seen |= front
-        front = { x for n in front for x in graph[n] if x not in seen }
-        print('front:', front)
-        print('seen:', seen)
-    return False
+            return True
+        elif not front:
+            return False;
+        else:
+            return testConnected(
+                {x for n in front for x in graph[n] if x not in seen},
+                seen | front
+            )
+    return testConnected(set([a]), set())
 
 print('isConnected(1, 5):', isConnected(graph, 1, 5))
 
